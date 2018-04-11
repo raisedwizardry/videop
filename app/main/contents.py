3ea0472b99a49d98374365638c9c4bb5c604d432
raisedwizardry <lifebloodasmr@gmail.com>
2018-04-10 21:09:44 -0400

    refactored clas show and added more methods to show

--- app/main/contents.py
+++ app/main/contents.py
@@ -27,21 +27,47 @@ class show:
 		plex = PlexServer(str(Config.PLEX_URL), str(Config.PLEX_TOKEN))
 		for video in plex.search(self.epshow):
 			#add logic to check for multiple search results
+			
 			if video.TYPE=='show':
 				lama=video.episode('',self.epseason,self.epnum)
-				self.plexdirfilename=lama.locations
+				plexdirfilename=lama.locations
+				#logic to compare filename with plexapi found filename
+				#if not plexdirfilename:
+					# add conditon to handle this
+				#if len(plexdirfilename)>1:
+					# add condition to handle this
+				if len(plexdirfilename)>0:
+					plexdirfilename=lama.locations[0]
+					if self.filename == os.path.basename(plexdirfilename): #if str(plexdirfilename) == str(self.dirfilename)
+						self.epname = '%s.%s.%s' % (lama.show().title, lama.seasonEpisode, lama.title)
 				
-				#add logic to compare filename with plex found filename maybe use len(lama.locations))
-				self.epname = '%s.%s.%s' % (lama.show().title, lama.seasonEpisode, lama.title)
+				
 	
 	def commit(self):
-		if self.filename == os.path.basename(self.plexdirfilename[0]):
-			writedb=Listed( directory=self.directory, dirfilename=self.dirfilename, 
-							filename=self.filename, filenamenoext=self.filenamenoext,
-							epnum=self.epnum, epseason=self.epseason,
-							epshow=self.epshow, epname=self.epname)
-			db.session.add(writedb)
-			db.session.commit()
+		writedb=Listed( directory=self.directory, dirfilename=self.dirfilename, 
+						filename=self.filename, filenamenoext=self.filenamenoext,
+						epnum=self.epnum, epseason=self.epseason,
+						epshow=self.epshow, epname=self.epname)
+		db.session.add(writedb)
+		db.session.commit()
+	
+	def isplaying(self):
+		plex = PlexServer(str(Config.PLEX_URL), str(Config.PLEX_TOKEN))
+		playing= plex.isPlayingMedia(includePaused=True)
+		
+
+	def retrieve(self, dirfilename):
+		entry=Listed.query.filter_by(dirfilename=str(dirfilename)).first()
+		if entry:
+			self.directory = entry.directory
+			self.dirfilename = entry.dirfilename
+			self.filename = entry.filename
+			self.filenamenoext = entry.filenamenoext
+			self.epnum = entry.epnum
+			self.epseason = entry.epseason
+			self.epshow = entry.epshow
+			self.epname = entry.epname
+		
 		#create a working directory
 		#create a log file
 		#move target file into directory
--- app/main/views.py
+++ app/main/views.py
@@ -11,40 +11,37 @@ from .contents import show
 
 @main.route('/', methods=['GET', 'POST'])
 def index():
-    form = PlexForm()
-    directory = Userconfig.query.get(1)
-    
-    if form.validate_on_submit():
-        #concheck = Userconfig.query.filter_by(plexdir=form.directory.data).first()
-        #if concheck == directory:
-       #     pass
-       # else:
-        concheck = Userconfig(plexdir=form.directory.data)
-        db.session.add(concheck)
-        db.session.commit()
-        return redirect(url_for('.index'))
-    return render_template('index.html',
-                           form=form, 
-                           directory=directory)
+	form = PlexForm()
+	directory = Userconfig.query.get(1)
+
+	if form.validate_on_submit():
+		#add check for input already in database and everwrite it if there. maybe this #concheck = Userconfig.query.filter_by(plexdir=form.directory.data).first()
+		concheck = Userconfig(plexdir=form.directory.data)
+		db.session.add(concheck)
+		db.session.commit()
+		return redirect(url_for('.index'))
+	return render_template('index.html',
+							form=form, 
+							directory=directory)
 
 #/mnt/d/Stuff/videop-dev-folder/Video/DVR/processing/plex-recordings
 @main.route('/list', methods=['GET','POST'])
 def listfiles():
-    files=list()
-    directory = Userconfig.query.get(1)
-    if directory is None:
-        pass
-    else:
-        for root, directories, filenames in os.walk(str(directory.plexdir)):
-            directories[:] = [d for d in directories if not d.startswith('.')]
-            for filename in filenames:
-                if filename.lower().endswith('.ts'):
-                    fill=show(root, filename)
-                    show.match(fill)
-                    dircheck = Listed.query.filter_by(dirfilename=fill.dirfilename).first()
-                    if dircheck is None:
-                        show.commit(fill)
-                    files.append(str(fill.epname))
-    return render_template('list.html', files=files, directory=directory)
+	files=list()
+	directory = Userconfig.query.get(1)
+	if directory is None:
+		pass
+	else:
+		for root, directories, filenames in os.walk(str(directory.plexdir)):
+			directories[:] = [d for d in directories if not d.startswith('.')]
+			for filename in filenames:
+				if filename.lower().endswith('.ts'):
+					fill=show(root, filename)
+					show.match(fill)
+					dircheck = Listed.query.filter_by(dirfilename=fill.dirfilename).first()
+					if dircheck is None:
+						show.commit(fill)
+					files.append(str(fill.epname))
+	return render_template('list.html', files=files, directory=directory)
 #/srv/samba/E10TB/Video/DVR/processing/plex-recording/tv-shows/A.P. BIO/Season 01/A.P. Bio (2018) - S01E05 - Dating Toledoans.ts
 #/mnt/d/Stuff/videop-dev-folder/Video/DVR/processing/plex-recordings/tv-shows/A.P. BIO/Season 01/A.P. Bio (2018) - S01E05 - Dating Toledoans.ts
